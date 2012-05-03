dc415bbac2cdad3ae79f42dba34ab55fec634e91
Chuck White <chuckwhite1@gmail.com>
2012-05-03 09:34:12 -0700

    finish user edit, update, index and destroy

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ source 'http://rubygems.org'
 gem 'rails', '3.2.3'
 gem 'bootstrap-sass','2.0.0'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'faker', '1.0.1'
+gem 'will_paginate', '3.0.3'
+gem 'bootstrap-will_paginate', '0.0.5'
+
 gem 'ffi','1.0.9'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -29,6 +33,7 @@ gem 'jquery-rails', '2.0.0'
 group :test do
    gem 'capybara','1.1.2'
    gem 'factory_girl_rails', '1.4.0'
+   gem 'database_cleaner','0.7.0'
 end
 group :production do
    gem 'pg','0.12.2'
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 describe "Authentication" do
 subject { page }
 describe "signin" do
+
 	before { visit signin_path }
 	describe "with invalid information" do
 	before { click_button "Sign in" }
@@ -22,8 +23,33 @@ describe "signin" do
 	end
 	it { should have_selector('title', text: user.name) }
 	it { should have_link('Profile', href: user_path(user)) }
+	it { should have_link('Settings', href: edit_user_path(user)) }
 	it { should have_link('Sign out', href: signout_path) }
 	it { should_not have_link('Sign in', href: signin_path) }
 end
+end
+describe "authorization" do
+	describe "for non-signed-in users" do
+		let(:user) { FactoryGirl.create(:user) }
+		describe "when attempting to visit a protected page" do
+			before do
+				visit edit_user_path(user)
+				fill_in "Email", with: user.email
+				fill_in "Password", with: user.password
+				click_button "Sign in"
+			end
+			describe "after signing in" do
+				it "should render the desired protected page" do
+				page.should have_selector('title', text: 'Edit user')
+				end
+			end
+		end
+	end
+	describe "in the Users controller" do
+		describe "visiting the user index" do
+			before { visit users_path }
+			it { should have_selector('title', text: 'Sign in') }
+		end
+	end
 end
 end
