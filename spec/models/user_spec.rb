da8eb456bd597b0037dcd10ea00a6a5b4fd33802
Chuck White <chuckwhite1@gmail.com>
2012-05-04 13:41:44 -0700

    add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,29 @@
 require 'spec_helper'
 
 describe User do
-before do
-@user = User.new(name: "Example User", email: "user@example.com",
+	before do
+	@user = User.new(name: "Example User", email: "user@example.com",
 				password: "foobar", password_confirmation: "foobar")
-end
-subject { @user }
-it { should respond_to(:name) }
-it { should respond_to(:email) }
-it { should respond_to(:password_digest) }
-it { should respond_to(:password) }
-it { should respond_to(:password_confirmation) }
-it { should respond_to(:remember_token)}
-it { should respond_to(:admin) }
-it { should respond_to(:authenticate) }
-it { should be_valid }
-it { should respond_to(:authenticate) }
-it { should respond_to(:microposts) }
+	end
+	subject { @user }
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token)}
+	it { should respond_to(:admin) }
+	it { should respond_to(:authenticate) }
+	it { should be_valid }
+	it { should respond_to(:authenticate) }
+	it { should respond_to(:microposts) }
+	it { should respond_to(:feed) }
+	it { should respond_to(:relationships) }
+	it { should respond_to(:followed_users) }
+	it { should respond_to(:reverse_relationships) }
+	it { should respond_to(:followers) }
+	it { should respond_to(:following?) }
+	it { should respond_to(:follow!) }
 describe "micropost associations" do
 	before { @user.save }
 	let!(:older_micropost) do
@@ -33,6 +40,24 @@ describe "micropost associations" do
 		@user.destroy
 		microposts.each do |micropost|
 			Micropost.find_by_id(micropost.id).should be_nil
+		end
+	end
+	describe "status" do
+		let(:unfollowed_post) do
+			FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+		end
+		let(:followed_user) { FactoryGirl.create(:user) }
+		before do
+			@user.follow!(followed_user)
+			3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+		end
+		its(:feed) { should include(newer_micropost) }
+		its(:feed) { should include(older_micropost) }
+		its(:feed) { should_not include(unfollowed_post) }
+		its(:feed) do
+			followed_user.microposts.each do |micropost|
+				should include(micropost)
+			end
 		end
 	end
 end
@@ -107,7 +132,25 @@ describe "return value of authenticate method" do
 	end
 	end
 describe "remember token" do
-before { @user.save }
-its(:remember_token) { should_not be_blank }
+	before { @user.save }
+	its(:remember_token) { should_not be_blank }
+end
+describe "following" do
+	let(:other_user) { FactoryGirl.create(:user) }
+	before do
+		@user.save
+		@user.follow!(other_user)
+	end
+	it { should be_following(other_user) }
+	its(:followed_users) { should include(other_user) }
+	describe "followed user" do
+		subject { other_user }
+		its(:followers) { should include(@user) }
+	end
+	describe "and unfollowing" do
+		before { @user.unfollow!(other_user) }
+		it { should_not be_following(other_user) }
+		its(:followed_users) { should_not include(other_user) }
+	end
 end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,23 +22,33 @@ describe "Home page" do
 			sign_in user
 			visit root_path
 		end
-	it "should render the user's feed" do
-		user.feed.each do |item|
+		
+		it "should render the user's feed" do
+			user.feed.each do |item|
 			page.should have_selector("li##{item.id}", text: item.content)
+			end
 		end
+		describe "follower/following counts" do
+			let(:other_user) { FactoryGirl.create(:user) }
+			before do
+				other_user.follow!(user)
+				visit root_path
+			end
+			it { should have_link("0 following", href: following_user_path(user)) }
+			it { should have_link("1 follower", href: followers_user_path(user)) }
 		end
 	end
 	end
 describe "Help page" do
-it "should have the h1 'Help'" do
-visit help_path
-page.should have_selector('h1', text: 'Help')
-end
-it "should have the title 'Help'" do
-visit help_path
-page.should have_selector('title',
-text: "Ruby on Rails Tutorial Sample App | Help")
-end
+	it "should have the h1 'Help'" do
+		visit help_path
+		page.should have_selector('h1', text: 'Help')
+	end
+	it "should have the title 'Help'" do
+		visit help_path
+		page.should have_selector('title',
+					text: "Ruby on Rails Tutorial Sample App | Help")
+	end
 end
 describe "About page" do
 it "should have the h1 'About'" do
